5c410355c1a1d0f0984379f4b8a3f36fa73fc57f
Madeleine <madeleinerclarke@gmail.com>
2022-11-18 15:02:06 -0800

    storing section 5 scratchwork

--- source/exploring.R
+++ source/exploring.R
@@ -28,3 +28,38 @@ p <- data %>%
     plot.title = element_text(size=15)
   )
 p
+
+# ----------------------------------------------------------------------------
+# Section 5 unused work:
+
+#Black percent prison vs percent gen public 
+b_data <- incarceration %>% 
+  filter(year == 2018) %>% 
+  select(state, county_name, division, total_pop_15to64, total_jail_pop, black_pop_15to64, black_jail_pop) %>% 
+  mutate(black_pop_percent = (black_pop_15to64/total_pop_15to64)*100, 
+         black_jail_percent = (black_jail_pop/total_jail_pop)*100) %>% 
+  filter(black_jail_percent <= 100)
+View(b_data)
+
+plot <- ggplot(b_data) +
+  geom_point(mapping = aes(x = black_pop_percent, y = black_jail_percent, 
+                           color = division),
+             alpha = 0.8) 
+plot
+
+plot <- ggplotly(plot)
+plot
+
+#White pop vs jail pop 
+w_data <- incarceration %>% 
+  filter(year == 2018) %>% 
+  select(state, county_name, division, total_pop_15to64, total_jail_pop, white_pop_15to64, white_jail_pop) %>% 
+  mutate(white_pop_percent = (white_pop_15to64/total_pop_15to64)*100, 
+         white_jail_percent = (white_jail_pop/total_jail_pop)*100) %>% 
+  filter(white_jail_percent <= 100)
+View(w_data)
+plot <- ggplot(w_data) +
+  geom_point(mapping = aes(x = white_pop_percent, y = white_jail_percent, 
+                           color = division),
+             alpha = 0.8) 
+plot
